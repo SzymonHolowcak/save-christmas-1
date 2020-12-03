29cefeb9a17f73e9c0e64ea96323682e595649d2
MohammedBrunfinger <szholowczak@gmail.com>
2020-12-03 09:06:05 +0100

    wow

--- statistics.js
+++ statistics.js
@@ -1,12 +1,30 @@
 
-function max(arr) {
-
+function max(arr) { 
+    let maxNum = arr[0]
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxNum) {
+            maxNum = arr[i]
+        }   
+    }
+        
+    return maxNum
 }
 
 function min(arr) {
-
+    let minNum = arr[0]
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < minNum) {
+            minNum = arr[i]
+        }   
+    }
+        
+    return minNum
 }
 
 function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+    
 }
+console.log(max([2,9,7,20,8,100,3]))
+console.log(min([2,9,7,20,8,100,3]))
